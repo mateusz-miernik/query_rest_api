6ab2d4f37bc436a5164d08f799afc273c6767b4a
Mateusz <mateusz.miernik1@gmail.com>
2021-04-06 12:05:32 +0200

    Updating main script

--- query_rest_api.py
+++ query_rest_api.py
@@ -1,26 +1,42 @@
 """
     Commandline script for simple REST API query.
-
 """
-import requests
+import os
 import json
 from argparse import ArgumentParser
+
+import requests
 
 
 def main():
     parser = ArgumentParser(description="Takes one keyword argument of mac address.")
-    parser.add_argument("-mac", type=str, action="store", default="", metavar="MAC",
+    parser.add_argument("--api-key", '-ak', type=str, action="store", default="", metavar="API_KEY",
+                        help="Define your valid API key.")
+    parser.add_argument("--mac", '-m', type=str, action="store", default="", metavar="MAC",
                         help="Define mac address for query action.")
     args = parser.parse_args()
     mac_address = args.mac
-    if mac_address == "":
+    api_key = args.api_key
+
+    if api_key == "":
+        print("No definition of API key was provided. Exiting...")
+        exit(1)
+    elif mac_address == "":
         print("No definition of mac address was provided. Exiting...")
-        return 1
+        exit(1)
 
     print(f"You provided this mac address to make a query: {mac_address}")
 
-    r = requests.get(r"https://api.macaddress.io/v1?apiKey=at_Y73ZuLrBFF6Ojg0j0y6dpoud1A6g9&output=json&search="
-                     + str(args.mac))
+    api_key = os.getenv('MACADDRESS_API_KEY')
+    r = requests.get(f"https://api.macaddress.io/v1?apiKey={api_key}&output=json&search={mac_address}")
+
+    if r.status_code != 200:
+        try:
+            r.raise_for_status()
+        except Exception as e:
+            print(e)
+        print(f"Something went wrong. Status code is {r.status_code}. \nExiting...")
+        exit(1)
 
     response_dict = json.loads(r.text)
     company_name = response_dict['vendorDetails']['companyName']
