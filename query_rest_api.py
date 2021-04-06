a8a0f7b22a26f84ac14aff671c78c0115ee4b986
Mateusz <mateusz.miernik1@gmail.com>
2021-04-06 10:40:14 +0200

    Updating main script

--- query_rest_api.py
+++ query_rest_api.py
@@ -6,17 +6,26 @@ import requests
 import json
 from argparse import ArgumentParser
 
-parser = ArgumentParser(description="Takes one keyword argument of mac address.")
-parser.add_argument("-mac", type=str, action="store", default="", metavar="MAC",
-                    help="Define mac address for query action.")
-args = parser.parse_args()
-mac_address = args.mac
-print(f"You provided this mac address to make a query: {mac_address}")
 
-r = requests.get(r"https://api.macaddress.io/v1?apiKey=at_Y73ZuLrBFF6Ojg0j0y6dpoud1A6g9&output=json&search="
-                 + str(args.mac))
+def main():
+    parser = ArgumentParser(description="Takes one keyword argument of mac address.")
+    parser.add_argument("-mac", type=str, action="store", default="", metavar="MAC",
+                        help="Define mac address for query action.")
+    args = parser.parse_args()
+    mac_address = args.mac
+    if mac_address == "":
+        print("No definition of mac address was provided. Exiting...")
+        return 1
 
-request_content = r.content
-response_dict = json.loads(r.text)
-company_name = response_dict['vendorDetails']['companyName']
-print(f"Company name for provided mac address is: {company_name}")
+    print(f"You provided this mac address to make a query: {mac_address}")
+
+    r = requests.get(r"https://api.macaddress.io/v1?apiKey=at_Y73ZuLrBFF6Ojg0j0y6dpoud1A6g9&output=json&search="
+                     + str(args.mac))
+
+    response_dict = json.loads(r.text)
+    company_name = response_dict['vendorDetails']['companyName']
+    print(f"Company name for provided mac address is: {company_name}")
+
+
+if __name__ == "__main__":
+    main()
